b346727f92bf4297dde094c6776ce973e542b01e
colleensvelasco <colleensvelasco@gmail.com>
2024-05-07 22:41:16 -0700

    edited DMQ.sql file and treatmentOrdersAndOrderedTreatments.html file

--- DMQ.sql
+++ DMQ.sql
@@ -15,7 +15,7 @@ SELECT providerID, name FROM Providers
 
 -- CREATE - Adds new patients 
 INSERT INTO Patients (patientName, birthDate, email, assignedProvider)
-    VALUES (:patientName, :birthDateInput, :emailInput, :assignedProvider_id_from_dropdown_Input)
+    VALUES (:patientNameInput, :birthDateInput, :emailInput, :assignedProvider_id_from_dropdown_Input)
 
 -- DELETE - Deletes a patient
 DELETE FROM Patients WHERE patientID = :patient_ID_selected_from_browse_patients_page
@@ -40,7 +40,7 @@ SELECT specialtyID, specialtyName FROM Specialties
 
 -- CREATE - Add a new specialty
 INSERT INTO Specialties (specialtyName)
-    VALUES (:specialtyName)
+    VALUES (:specialtyNameInput)
 
 
 
@@ -58,7 +58,7 @@ SELECT specialtyID, specialtyName FROM Specialties
 
 -- CREATE - Adds new provider
 INSERT INTO Providers (name, dentalSpecialty)
-    VALUES (:name, :dentalSpecialty_id_from_dropdown_Input)
+    VALUES (:nameInput, :dentalSpecialty_id_from_dropdown_Input)
 
 
 
@@ -69,10 +69,44 @@ SELECT treatmentID, treatmentName, treatmentDesc, cost FROM Treatments
 
 -- CREATE - Adds new treatment
 INSERT INTO Treatments (treatmentName, treatmentDesc, cost)
-    VALUES (:treatmentName, :treatmentDesc, :cost)
+    VALUES (:treatmentNameInput, :treatmentDescInput, :costInput)
 
 
 
----- TREATMENT ORDERS ----
+---- TREATMENT ORDERS AND TREATMENT_TREATMENTORDERS ----
 
+-- SELECT - Show all treatment orders
+SELECT treatmentOrderID, patientID, dateTx, totalCost FROM TreatmentOrders
+
+-- SELECT - Show all ordered treatments
+SELECT treatmentOrderID, treatmentID FROM Treatments_TreatmentOrders
+
+-- CREATE TREATMENT ORDER WITHOUT TREATMENTS - Adds treatment order without treatments
+-- Total Cost is 0
+INSERT INTO TreatmentOrders (patientID, dateTx, totalCost)
+    VALUES (:patientIDInput, :dateTxInput, 0)
+
+-- Treatment Dropdown (gets all treatmentIDs and names to populate provider dropdown) when adding
+-- new treatment_treatmentorders (new treatments to specified treatment order)
+SELECT treatmentID, treatmentName FROM Treatments
+
+-- CREATE TREATMENT_TREATMENTORDER - Adds treatment order with treatments
+INSERT INTO Treatments_TreatmentOrders (treatmentOrderID, treatmentID)
+    VALUES (:treatmentOrderIDgenerated, :treatment_ID_from_dropdown_Input)
+
+-- UPDATE TREATMENT_TREATMENTORDER - Updates treatment on treatment order
+SELECT treatmentOrderID, treatmentID
+    FROM Treatments_TreatmentOrders
+    WHERE treatmentOrderID = :treatmentOrder_ID_selected_from_browse_ordertx_page AND 
+        treatmentID = :treatment_ID_selected_from_browse_ordertx_page
+
+-- SAVE -> Updates TREATMENT_TREATMENTORDER
+UPDATE Treatments_TreatmentOrders
+    SET treatmentID = :treatment_ID_from_dropdown_Input
+    WHERE treatmentOrderID = :treatmentOrder_ID_selected_from_browse_ordertx_page AND 
+        treatmentID = :treatment_ID_selected_from_browse_ordertx_page
+
+-- DELETE TREATMENT_TREATMENTORDER
+DELETE FROM Treatments_TreatmentOrders WHERE treatmentOrderID = :treatmentOrder_ID_selected_from_browse_ordertx_page AND 
+        treatmentID = :treatment_ID_selected_from_browse_ordertx_page
 
